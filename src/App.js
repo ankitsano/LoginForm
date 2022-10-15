e204bb7d8a47e30d27b733d43b1ab03531d0713f
ankit <ankitsanodiya789@gmail.com>
2022-10-15 09:07:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-import React, { useState, useEffect } from "react";
-import Amazon from "./components/amazon";
-import Navbar from "./components/navbar";
-import Cart from "./components/cart";
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
-const App = () => {
-  const [show, setShow] = useState(true);
-  const [cart, setCart] = useState([]);
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
 
-  const handleClick = (item) => {
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
-  };
-
-  const handleChange = (item, d) => {
-    const ind = cart.indexOf(item);
-    const arr = cart;
-    arr[ind].amount += d;
-
-    if (arr[ind].amount === 0) arr[ind].amount = 1;
-    setCart([...arr]);
-  };
-
-  // useEffect(() => {
-  //   console.log("cart change");
-  // }, [cart]);
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
   return (
-    <React.Fragment>
-      <Navbar setShow={setShow} size={cart.length} />
-      {show ? (
-        <Amazon handleClick={handleClick} />
-      ) : (
-        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
-      )}
-    </React.Fragment>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
   );
-};
+}
 
 export default App;
